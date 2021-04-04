26c21cec27f0b18d0bbe249bf5af9ac2558d36ae
Damien Butt <damien.butt@norgateav.com>
2021-04-04 17:24:46 +0100

    chore: add data provider and react-admin wrapper

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-    return <div className="App"></div>;
-}
+import React from "react";
+import { Admin, Resource } from "react-admin";
+import simpleRestProvider from "ra-data-simple-rest";
+
+const dataProvider = simpleRestProvider("http://localhost:3000");
+
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users" />
+    </Admin>
+);
 
 export default App;
